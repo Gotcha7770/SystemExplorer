bcf17f6ea7db8f7ebb600c4d4c8b50d0595bbc5d
Mark Patka <Mark.Kaz.1998@gmail.com>
2023-11-29 22:48:30 +0300

    Adds Folder and Project For the Upcoming Tests

---
+++ SystemExplorer.Tests/Directories/CreateDirectoryTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace SystemExplorer.Tests.Directories;
+
+public class CreateDirectoryTest
+{
+    [Fact]
+    public void CreateDirectoryTest_Success()
+    {
+        // ARRANGE
+            /// Создать VMFactory -> Получить комманду CreateDirectoryCommand
+
+        string[] pathes = new[] 
+        {
+            @"D:\test_folder\папка",
+            @"D:\test_folder",
+            @"D:\",
+            string.Empty
+        };
+        // ACT 
+
+
+        // ASSERT
+        
+    }
+}
